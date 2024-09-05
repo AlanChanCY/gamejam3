24fb85b8b9a07e9b9cfb6dbc1c40b8e2763e6569
AlanChanCY <141455795+AlanChanCY@users.noreply.github.com>
2024-09-05 01:38:16 -0400

    Initial commit

---
+++ Assets/GM.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+    public Transform text;
+    public Transform text2;
+    private CanvasGroup underText;
+    public float nextTime = 0;
+    float interval = 0.5f;
+    public static int HighScore;
+    public string phrase = "";
+    
+    // Use this for initialization
+    void Start () {
+             underText = text2.GetComponent<CanvasGroup>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+            //Debug.Log(player.getScore());
+            text.SendMessage("updateText",phrase );
+
+    }
+    public int getHighScore()
+    {
+        return HighScore;
+    }
+    public void setHighScore(int HighScores)
+    {
+        HighScore = HighScores;
+    }
+    public void gameOver(int highScore)
+    {
+        HighScore = highScore;
+        Application.LoadLevel("GameOver");
+    }
+    public void setPhrase(string phrases)
+    {
+        phrase = phrases;
+    }
+        
+
+}
